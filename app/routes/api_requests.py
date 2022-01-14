8ff46a307b7f9a6795cef2a6b439e37371c26ae4
Erica Siegel <siegel.erica@gmail.com>
2022-01-13 16:27:58 -0800

    added dog breed api info to homepage

--- app/routes/api_requests.py
+++ app/routes/api_requests.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+breed_info = {}
+
 def breed_names():
     """
     function to return the list of dog breeds and ids from dog api
@@ -14,7 +16,7 @@ def breed_names():
         name = n['name']
         dog_id = n['id']
         return name, dog_id
-        
+     
 def breed_stats(id):
     """
     function to return the json data for a single dog breed. After getting the json data
@@ -27,22 +29,32 @@ def breed_stats(id):
     data = json.loads(response.text)[0]
     # print(data)
     name = data['breeds'][0]['name']
-    print('name: ', name)
+    breed_info['name']=name
+    # print('name: ', name)
     weight = data['breeds'][0]['weight']['imperial']
-    print('weight(lbs): ', weight)
+    breed_info['weight']=weight
+    # print('weight(lbs): ', weight)
     height = data['breeds'][0]['height']['imperial']
-    print('height(inches): ', height)
+    breed_info['height']=height
+    # print('height(inches): ', height)
     use = data['breeds'][0]['bred_for']
-    print('use: ', use)
+    breed_info['use']=use
+    # print('use: ', use)
     group = data['breeds'][0]['breed_group']
-    print('breed group: ', group)
+    breed_info['group']=group
+    # print('breed group: ', group)
     temp = data['breeds'][0]['temperament']
-    print('temperament: ', temp)
+    breed_info['temp']=temp
+    # print('temperament: ', temp)
     lifespan = data['breeds'][0]['life_span']
-    print('life span: ', lifespan)
+    breed_info['lifespan']=lifespan
+    # print('life span: ', lifespan)
     image = data['url']
-    print('image: ', image)
+    breed_info['image']=image
+    # print('image: ', image)
+    # return name, weight, height, use, group, temp, lifespan, image
     
-    
+breed_stats(29)  
+print(breed_info)  
     
 
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from .api_requests import breed_info as stats
 
 # Blueprint() lets us consolidate routes into a single bp object
 bp = Blueprint('home', __name__, url_prefix='/')
@@ -6,7 +7,8 @@ bp = Blueprint('home', __name__, url_prefix='/')
 # route to index.html
 @bp.route('/')
 def index():
-    return render_template('homepage.html')
+    breed = stats['name']
+    return render_template('homepage.html', breed=breed)
 # login page route
 @bp.route('/login')
 def login():
