3f56590d12697e8c656446322a16b9ff3adc6918
Erica Siegel <siegel.erica@gmail.com>
2022-01-13 17:34:02 -0800

    created card to display dog stats

--- app/routes/home.py
+++ app/routes/home.py
@@ -8,7 +8,23 @@ bp = Blueprint('home', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     breed = stats['name']
-    return render_template('homepage.html', breed=breed)
+    use = stats['use']
+    height = stats['height']
+    weight = stats['weight']
+    temp = stats['temp']
+    lifespan = stats['lifespan']
+    group = stats['group']
+    img = stats['image']
+    return render_template('homepage.html', 
+                           breed=breed, 
+                           use=use, 
+                           height=height,
+                           weight=weight,
+                           temp=temp,
+                           lifespan=lifespan,
+                           group=group,
+                           img=img
+                           )
 # login page route
 @bp.route('/login')
 def login():
