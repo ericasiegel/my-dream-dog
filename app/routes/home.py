20775c1f1432e54a4f762928b9dc9fae2c6e65b5
Erica Siegel <siegel.erica@gmail.com>
2022-01-14 16:50:40 -0800

    simplified index route code

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from .api_requests import breed_info as stats
-from .api_requests import breed_list as bList
+from .api_requests import breed_list as breeds
 
 # Blueprint() lets us consolidate routes into a single bp object
 bp = Blueprint('home', __name__, url_prefix='/')
@@ -8,28 +8,10 @@ bp = Blueprint('home', __name__, url_prefix='/')
 # route to index.html
 @bp.route('/')
 def index():
-    # dropdown
-    breeds = bList
-    # print(breeds)
-    # breed card info
-    breed = stats['name']
-    use = stats['use']
-    height = stats['height']
-    weight = stats['weight']
-    temp = stats['temp']
-    lifespan = stats['lifespan']
-    group = stats['group']
-    img = stats['image']
     return render_template('homepage.html', 
-                        #    card info
-                           breed=breed, 
-                           use=use, 
-                           height=height,
-                           weight=weight,
-                           temp=temp,
-                           lifespan=lifespan,
-                           group=group,
-                           img=img,
+                        # card info
+                           stats=stats,
+                       
                         # dropdown info
                            breeds=breeds
                            )
