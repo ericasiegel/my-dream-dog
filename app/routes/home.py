4a691afd35523187ab611e829bfa1e3340800f0a
Erica Siegel <siegel.erica@gmail.com>
2022-01-19 21:50:55 -0800

    added conditional for first page load without form submission

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,7 +1,7 @@
 from crypt import methods
 from tkinter.tix import Select
 from flask import Blueprint, render_template, request
-from django.shortcuts import render
+import random
 
 # import files
 from .api_requests import breed_stats
@@ -14,18 +14,19 @@ bp = Blueprint('home', __name__, url_prefix='/')
 # route to index.html
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-
-    # try:
-    #     random_num = random.randint(1, 264)
-    #     breed_stats(random_num)
-    # except:
-    #     breed_stats(165)
-
+    # if 'Submit' != 'Submit':
+    
     if request.method == 'POST':
         # breed_id = data.value
         breed_id = list(request.form.listvalues())[0][0]
-        print(breed_id)
+        # print(breed_id)
         breed_stats(breed_id)
+    else:
+        try:
+            random_num = random.randint(1, 264)
+            breed_stats(random_num)
+        except:
+            breed_stats(165)
     
     return render_template('homepage.html', 
                         # card info
