45717f02fb426d87376f850435d5a34d003ec754
Erica Siegel <siegel.erica@gmail.com>
2022-01-21 16:38:58 -0800

    fixed breed card and dashboard container

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
 
+from .api_requests import breed_stats
+from .api_requests import breed_info as stats
+
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp.route('/')
 def dash():
-    return render_template('dashboard.html')
+    breed_stats(165)
+    return render_template('dashboard.html', 
+                            # card info
+                           stats=stats
+                           )
