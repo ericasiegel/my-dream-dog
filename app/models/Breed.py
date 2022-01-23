ea831bf1c0cdc5a24941b17dfcbe15d72c71cf0a
Erica Siegel <siegel.erica@gmail.com>
2022-01-23 00:13:54 -0800

    added the Breed class for the database

---
+++ app/models/Breed.py
@@ -0,0 +1,11 @@
+from app.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, null
+from sqlalchemy.orm import relationship
+
+class Breed(Base):
+    __tablename__ = 'breeds'
+    id = Column(Integer, primary_key=True)
+    breed_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    
+    user = relationship('User')
