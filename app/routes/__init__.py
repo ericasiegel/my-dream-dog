2fb319d86ffab9997a348e466dfad5db46104ef2
Erica Siegel <siegel.erica@gmail.com>
2022-01-25 19:51:35 -0800

    new user information submitted to database

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,4 @@
 # .home syntax directs the program to find the module named home in the current directory(.)
 from .home import bp as home
 from .dashboard import bp as dashboard
+from .api import bp as api
---
+++ app/routes/api.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, request, redirect, jsonify
+from app.models import User
+from app.db import get_db
+
+bp = Blueprint('api', __name__, url_prefix='/api')
+
+@bp.route('/users', methods=['POST'])
+def signup():
+    db = get_db()
+    
+    try:
+        if request.method == 'POST':
+            # get the data submitted through form
+            data = request.form
+            
+            # create a new user
+            newUser = User(
+                username = data['username'],
+                email = data['email'],
+                password = data['password']
+            )
+            
+            # save in the database
+            db.add(newUser)
+            db.commit()
+            
+            #redirect user to dashboard after form is submitted
+            return redirect('/dashboard')
+    except:
+        # insert failed, so send error to front end
+        return jsonify(message = 'Signup failed'), 500
