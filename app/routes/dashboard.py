62f8b8ed158676496fec27f0ba5faeb8ca1ce8a1
Erica Siegel <siegel.erica@gmail.com>
2022-01-28 13:57:37 -0800

    conditionally rendered dashboard breed card

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, request
 from app.models import Breed, User
 from app.db import get_db
+import random
 from .api_requests import breed_stats
 from .api_requests import breed_info as stats
 
@@ -15,15 +16,39 @@ def dash():
         .filter(Breed.user_id == session.get('user_id'))
         .all()
     )
+    attribute = 'hidden'
+    message = ''
     if request.method == 'POST':
-        data = request.form
-        breed_stats(data['id'])
+            data = request.form
+            breed_stats(data['id'])
+            attribute = ''
+            message = 'hidden'
+            
+    else:
+        attribute = 'hidden'
+        message = ''
+        
+    
+    
+    
+    # if request.method == 'POST':
+    #     data = request.form
+    #     breed_stats(data['id'])
+    # else:
+    #     try:
+    #         random_num = random.randint(1, 264)
+    #         breed_stats(random_num)
+    #     except:
+    #         breed_stats(200)
+ 
         
     return render_template('dashboard.html', 
                             # card info
                            stats=stats,
                            # saved breed names
                            saved=single_id,
+                            attribute=attribute,
+                            message=message,
                            #log session info
                             loggedIn=session.get('loggedIn')
                            )
