4770255f2a08e5e0980b59f78dfaf39c47dd5d06
Erica Siegel <siegel.erica@gmail.com>
2022-01-30 00:23:36 -0800

    randomized remaining dog ids

--- app/routes/quiz.py
+++ app/routes/quiz.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request, session, redirect, jsonif
 # from .temp_list import temperament_list as tl
 import requests
 import json
+import random
 # from .api_requests import
 
 # Blueprint
@@ -27,17 +28,17 @@ def get_size(lbs):
                 s['id'] = n['id']
                 s['temperament'] = n['temperament']
                 dog_sizes.append(s)
-                print('small')
+                # print('small')
             elif (size >= 23 and size <= 55) and (lbs >= 23 and lbs <= 55):
                 s['id'] = n['id']
                 s['temperament'] = n['temperament']
                 dog_sizes.append(s)
-                print('medium')
+                # print('medium')
             elif lbs >= 56 and size >=56:
                 s['id'] = n['id']
                 s['temperament'] = n['temperament']
                 dog_sizes.append(s)
-                print('large')
+                # print('large')
     # print(len(dog_sizes))
     
 def get_temperament(*args):
@@ -46,37 +47,56 @@ def get_temperament(*args):
     """
     
     temperament_ids = []
+    new_temp_ids = []
     top_temps = []
     # print(args)
+    
+    # get the list of dog ids that match the temperament args
     for c in args:
         for t in dog_sizes:
             # print(t['breed_group'])
             if c in t["temperament"]:
                 temperament_ids.append(t['id'])
-    
-    print(sorted(temperament_ids))
+    # print(sorted(temperament_ids))
     st = sorted(temperament_ids)
+    
+    # find all of the ids if they occur more than 3 times
     for r in range(265):
         if r in st:
             x = st.count(r)
             if x >= 3:
-                top_temps.append(r)
+                new_temp_ids.append(r)
+                
+    # if the main list is less than 5 ids
+    # find all of the ids if they occur 2 times
     if len(top_temps) < 5:
         for r in range(265):
             if r in st:
                 x = st.count(r)
                 if x == 2:
-                    top_temps.append(r)
-                else:
-                    top_temps.append(r)
+                    new_temp_ids.append(r)
 
-    print(top_temps)
-    print(top_temps[:5])
-    # return top_temps[:5]
+    # if the main list is still less than 5 ids
+    # randomy shuffle the remaining ids and add to final list
+    if len(top_temps) < 5:
+        rest = []
+        for r in range(265):
+            if r in st:
+                x = st.count(r)
+                if x == 1:
+                    rest.append(r)
+        # print(rest)
+        random.shuffle(rest)
+        # print(rest)
+        top_temps = new_temp_ids + rest
+
+    # print(top_temps)
+    # print(top_temps[:5])
+    return top_temps[:5] #return the first 5 in list
 
 @bp.route('/', methods=['GET', 'POST'])
 def quiz():
     pass
 
-get_size(60)
+get_size(40)
 get_temperament('Strong', 'Quiet', 'Loving', 'Independent', 'Playful')
